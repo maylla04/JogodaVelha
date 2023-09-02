b8a13bbdeae95b5e7fc3f7d91ab1117540fc60ff
maylla04 <maylla.rie@gmail.com>
2023-09-01 21:43:29 -0400

    git init game

--- js/main.js
+++ js/main.js
@@ -16,3 +16,25 @@ var formas = ['X', 'O'];
 var index = null;
 
 var tabuleiro = new Array(9);
+
+ComecaJogo = function() {
+    var nomeJogador1 = document.getElementById('jogador1').value;
+    var nomeJogador2 = document.getElementById('jogador2').value;
+    jogador1 = new Jogador(nomeJogador1, 0); 
+    jogador2 = new Jogador(nomeJogador2, 1); 
+
+    jogadorAtual = jogador1;
+    setLabelJogadorAtual();
+
+    
+    document.getElementById('jogo').style.visibility = 'visible';
+    
+}
+
+
+reset = function() { window.location.reload(); }
+
+
+setLabelJogadorAtual = function() {
+    document.getElementById('jogadorAtual').innerHTML = 'Jogador atual:  ' + jogadorAtual.nome;
+}
