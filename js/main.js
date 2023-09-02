efcfea119993cdb322d78e3fd002c62ab7fec298
maylla04 <maylla.rie@gmail.com>
2023-09-01 21:40:27 -0400

    git update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ window.onload = () => {
         navigator.serviceWorker.register("./sw.js");
     }
 }
+
+function Jogador(nome, forma){
+    this.nome = nome;
+    this.forma = forma;
+}
+
+var jogador1, jogador2;
+var jogadorAtual;
+var formas = ['X', 'O'];
+var index = null;
+
+var tabuleiro = new Array(9);
